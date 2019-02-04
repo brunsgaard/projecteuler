3d013f746952c630093dd067e2bc3d4ad0044dbe
Jonas Brunsgaard <jonas.brunsgaard@gmail.com>
2019-02-05 00:01:13 +0100

    wip p1

--- Main.hs
+++ Main.hs
@@ -1,12 +1,10 @@
 #!/usr/bin/env stack
 -- stack --install-ghc runghc
 
-
-testdiv :: Int -> Bool
-testdiv n = mod n 3 == 0 || mod n 5 == 0
-
+euler1:: Int -> Int
 euler1 n = 
   let
+    testdiv n = mod n 3 == 0 || mod n 5 == 0
     x = [ x | x <- [1..n-1], testdiv x] 
   in
     foldl (+) 0 x
